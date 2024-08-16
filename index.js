27eea985d1969ae73cf405e5c706d522f50c542a
TDMWalpala <tharindudarshana2019@gmail.com>
2024-08-16 19:06:27 +0530

    Resolver & GQL as a data service

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const {ApolloServer,gql} = require('apollo-server');
+const employees = require('./data/employees.json');
 
 const typeDefs = gql`
      type Query {
@@ -9,12 +10,20 @@ const typeDefs = gql`
          firstName: String,
          lastName: String,
          destination: String,
-         department: String,
+         department: String  @deprecated ( reason:"Since company moving"),
          nearestCity: String
     }
     `;
 
-const gplServer  = new ApolloServer({typeDefs});
+const resolvers = {
+    Query: {
+        employees : ()=> {
+            return employees
+        }
+    }
+}
+
+const gplServer  = new ApolloServer({typeDefs, resolvers});
 
 gplServer.listen({port : process.env.PORT || 4000})
          .then(({url}) => console.log(`GraphQL server start on ${url}`));
