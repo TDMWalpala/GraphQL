c11c1c4b8df5f63f6d37f34e2bf9676123397e34
TDMWalpala <tharindudarshana2019@gmail.com>
2024-08-17 10:33:52 +0530

    GQL-2:Added Data Sources

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const {ApolloServer,gql} = require('apollo-server');
-const employees = require('./data/employees.json');
+const EmployeeService = require('./datasources/file');
 
 const typeDefs = gql`
      type Query {
-         employees:[Employee]
+        employees (
+            id: ID
+            firstName: String,
+            lastName: String,
+            destination: String,
+            department: String,
+            nearestCity: String
+        ):[Employee],
+        findEmployeeById(id:ID): Employee
+
      }
      type Employee {
          id: ID!
@@ -17,13 +26,21 @@ const typeDefs = gql`
 
 const resolvers = {
     Query: {
-        employees : ()=> {
-            return employees
+        employees : (parent, args, {dataSources}, info)=> {
+            return dataSources.employeeService.getEmployees(args);
+        },
+        findEmployeeById: (params, {id}, {dataSources}, info) => {
+            return dataSources.employeeService.getEmployeeById(id)[0];
         }
     }
 }
 
-const gplServer  = new ApolloServer({typeDefs, resolvers});
+const dataSources = ()=> ({
+    employeeService: new EmployeeService()
+
+});
+
+const gplServer  = new ApolloServer({typeDefs, resolvers, dataSources});
 
 gplServer.listen({port : process.env.PORT || 4000})
          .then(({url}) => console.log(`GraphQL server start on ${url}`));
